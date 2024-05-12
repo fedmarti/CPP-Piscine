421eef41dfba3416b129313bd9f9634ff758bd8f
fedmarti <fedmarti@student.42firenze.it>
2024-05-12 16:30:01 +0200

    changed code to fit with the subject

--- CPP_Module_08/ex00/easyfind.hpp
+++ CPP_Module_08/ex00/easyfind.hpp
@@ -6,20 +6,20 @@
 /*   By: fedmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 23:15:14 by fedmarti          #+#    #+#             */
-/*   Updated: 2024/05/12 00:41:14 by fedmarti         ###   ########.fr       */
+/*   Updated: 2024/05/12 16:28:53 by fedmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <exception>
+#include <algorithm>
 
 template <typename T>
 typename T::iterator	easyfind(T& container, int val) throw(std::exception)
 {
-	for (typename T::iterator it = container.begin(); it != container.end(); it++)
-	{
-		if (static_cast<int>(*it) == val)
-			return (it);
-	}
+	typename T::iterator it = std::find(container.begin(), container.end(), val);
+	if (static_cast<int>(*it) == val)
+		return (it);
+		
 	throw(std::exception()); 
 }
